82603b8602c09e86c2be17ead58507c436fb676e
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-06-05 21:39:43 +0200

    chore: update package.json

--- src/components/components/menu/menu.tsx
+++ src/components/components/menu/menu.tsx
@@ -2,12 +2,15 @@ import cn from 'classnames';
 import React from 'react';
 import modifiers, { ElementModifier } from '../../../modifiers';
 import { Element } from '../../elements';
+import { MenuList } from './menu-list';
 
 type MenuProps = ElementModifier;
 
-export const Menu: React.FunctionComponent<MenuProps> = ({ className, ...props }: MenuProps) => (
-  <Element {...props} className={cn('menu', className)} />
-);
+export const Menu: React.FunctionComponent<MenuProps> & {
+  List: typeof MenuList;
+} = ({ className, ...props }: MenuProps) => <Element {...props} className={cn('menu', className)} />;
+
+Menu.List = MenuList;
 
 Menu.defaultProps = {
   ...modifiers.defaultProps,
