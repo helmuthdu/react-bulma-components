8acb38264944e6d2dfc74e0ad6d1dcabe5039f8a
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-02 23:00:48 +0100

    fix: ignore stories for code coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testEnvironment: 'jest-environment-jsdom-fourteen',
   coverageDirectory: '<rootDir>/.coverage',
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.story.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.stories.{js,jsx,ts,tsx}'],
   coverageReporters: ['lcov', 'text', 'text-summary'],
   coverageThreshold: {
     global: {
