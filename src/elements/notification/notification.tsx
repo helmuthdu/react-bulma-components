42b7beec1bfb5b347052986e12c5f25d942dbfaa
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-03-23 18:11:46 +0100

    feat: support light/dark for notification components

--- src/elements/notification/notification.tsx
+++ src/elements/notification/notification.tsx
@@ -5,14 +5,18 @@ import modifiers, { ElementModifier } from '../../modifiers';
 import { Element } from '../element';
 
 type NotificationProps = ElementModifier & {
-  color?: Colors;
+  color?: Exclude<Colors, 'light' | 'dark'>;
+  dark?: boolean;
+  light?: boolean;
 };
 
 export const Notification = React.forwardRef<HTMLDivElement, NotificationProps>(
-  ({ children, className, color, ...props }, ref) => (
+  ({ children, className, color, dark, light, ...props }, ref) => (
     <Element
       ref={ref}
       className={clsx('notification', className, {
+        'is-dark': dark,
+        'is-light': light,
         [`is-${color}`]: color
       })}
       {...props}
